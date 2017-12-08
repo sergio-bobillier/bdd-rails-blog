7e7860a0fd915e0ec376bd0ae8c8774bd8bd2ade
Sergio Bobillier C <sergio.bobillier@gmail.com>
2017-12-08 18:59:39 -0500

    Implements the article editing feature.

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -63,3 +63,37 @@ RSpec.feature 'Creating Articles' do
     expect(page).to have_content("Body can't be blank")
   end
 end
+
+RSpec.feature 'Editing articles' do
+  before do
+    @article = Article.create(title: 'The first article', body: 'Body of the first article')
+  end
+
+  scenario 'A user edits an article' do
+    visit '/'
+
+    click_link @article.title
+    click_link 'Edit Article'
+
+    fill_in 'Title', with: 'Updated article'
+    fill_in 'Body', with: 'Updated body of article'
+    click_button 'Save'
+
+    expect(page).to have_content('Article has been updated')
+    expect(page.current_path).to eq(article_path(@article))
+  end
+
+  scenario 'A user fails to update an article' do
+    visit '/'
+
+    click_link @article.title
+    click_link 'Edit Article'
+
+    fill_in 'Title', with: ''
+    fill_in 'Body', with: 'Updated body of article'
+    click_button 'Save'
+
+    expect(page).to have_content('Article has not been updated')
+    expect(page.current_path).to eq(article_path(@article))
+  end
+end
