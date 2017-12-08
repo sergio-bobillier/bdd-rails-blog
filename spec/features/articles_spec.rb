66a8d180b9b98c17dabbb8c507e6abdbf2a043ad
Sergio Bobillier C <sergio.bobillier@gmail.com>
2017-12-08 16:56:45 -0500

    Implements the article list.

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
+RSpec.feature 'Listing articles' do
+  before do
+    @article1 = Article.create(title: 'The first article', body: 'Body of the first article')
+    @article2 = Article.create(title: 'The second article', body: 'Body of the second article')
+  end
+
+  scenario 'A user lists the articles' do
+    visit '/'
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+  end
+end
+
 RSpec.feature 'Creating Articles' do
   scenario 'A user creates a new article' do
     visit '/'
