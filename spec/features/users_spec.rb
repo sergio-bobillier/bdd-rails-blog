ce5a968497b36cb883e8f95d2cf1ca5ac61a1f7f
Sergio Bobillier C <sergio.bobillier@gmail.com>
2017-12-08 21:47:46 -0500

    Implements users sign-in.

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -14,3 +14,22 @@ RSpec.feature 'Users sign-up' do
     expect(page).to have_content('You have signed up successfully.')
   end
 end
+
+RSpec.feature 'Users sign-in' do
+  before do
+    @john = User.create!(email: 'john@example.com', password: 'password')
+  end
+
+  scenario 'Sign-in with valid credentials' do
+    visit '/'
+
+    click_link 'Sign in'
+
+    fill_in 'Email', with: @john.email
+    fill_in 'Password', with: @john.password
+    click_button 'Log in'
+
+    expect(page).to have_content 'Signed in successfully.'
+    expect(page).to have_content "Signed in as #{@john.email}"
+  end
+end
