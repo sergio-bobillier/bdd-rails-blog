4797b6a6c68a0d19edd5a4bb9094f3fd96dba41b
Sergio Bobillier C <sergio.bobillier@gmail.com>
2017-12-09 09:28:57 -0500

    Implements the users sign-out feature.

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -33,3 +33,22 @@ RSpec.feature 'Users sign-in' do
     expect(page).to have_content "Signed in as #{@john.email}"
   end
 end
+
+RSpec.feature 'Users sign-out' do
+  before do
+    @john = User.create!(email: 'john@example.com', password: 'password')
+
+    visit '/'
+    click_link 'Sign in'
+    fill_in 'Email', with: @john.email
+    fill_in 'Password', with: @john.password
+    click_button 'Log in'
+  end
+
+  scenario 'Sign-out after signing-in' do
+    visit '/'
+    click_link 'Sign out'
+
+    expect(page).to have_content('Signed out successfully.')
+  end
+end
